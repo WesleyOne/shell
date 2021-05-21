9b06b3ff1854314efe7bfa6803f9da026ac9b933
wesleyone <wesleyone666@163.com>
2021-05-22 01:22:16 +0800

    OSS命令上传文件

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # shell
 一些脚本
+
+## OSS命令上传文件（osscp.sh）
+
+> 使用限制:目前仅支持MacOS、CentOS、Ubuntu的64位/32位系统
+
+### 使用方法：
+
+1. 下载脚本
+
+2. 修改配置
+```sh
+# 访问OSS配置（以下四个变量必须替换成可用信息）
+# 填写Bucket所在地域的Endpoint，建议使用内网域名
+endpoint="oss-cn-hangzhou.aliyuncs.com"
+# 填写账号的AccessKey，尽量限制该账号的权限
+accessKeyID="***"
+accessKeySecret="***"
+# Bucket名称及其内的文件夹名称
+bucketAndFold="***/***/"
+```
+
+3. 脚本添加执行权限
+```sh
+chmod +x osscp.sh
+```
+
+4. 上传指定文件
+```sh
+./osscp.sh [filename1] [filename2] [filenameN]
+```
---
+++ osscp.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# 简化上传文件到OSS
+# 感谢官方工具(https://github.com/aliyun/ossutil)
+# 使用命令格式如下：
+# chmod +x osscp.sh
+# ./osscp.sh [filename1] [filename2] [filenameN]
+
+# 访问OSS配置（以下四个变量必须替换成可用信息）
+endpoint="oss-cn-hangzhou.aliyuncs.com"
+accessKeyID="***"
+accessKeySecret="***"
+bucketAndFold="***/***/"
+
+# 生成或覆盖配置文件
+conf="ossutilconf"
+echo "[Credentials]
+language=CH
+endpoint=${endpoint}
+accessKeyID=${accessKeyID}
+accessKeySecret=${accessKeySecret}" > ${conf}
+
+# 获取系统信息
+SYS_UNAME=`uname  -a`
+MAC_OS="Darwin"
+CENTOS="centos"
+UBUNTU_OS="ubuntu"
+LINUX_OS="linux"
+OS_BIT=`getconf LONG_BIT`
+
+if [[ $SYS_UNAME =~ $MAC_OS ]];then
+    _SYS=$MAC_OS
+elif [[ $SYS_UNAME =~ $CENTOS ]];then
+    echo "centos"
+    _SYS=$LINUX_OS
+elif [[ $SYS_UNAME =~ $UBUNTU_OS ]];then
+    echo "ubuntu"
+    _SYS=$LINUX_OS
+else
+    echo "[ERROR]未知系统 SYSTEM:"$SYS_UNAME
+    exit
+fi
+
+# 根据系统类型选择脚本
+if [[ $_SYS == $MAC_OS && $OS_BIT == 64 ]]; then
+	cmd=ossutilmac64
+elif [[ $_SYS == $MAC_OS && $OS_BIT == 32 ]]; then
+	cmd=ossutilmac32
+elif [[ $_SYS == $LINUX_OS && $OS_BIT == 64 ]]; then
+	cmd=ossutil64
+elif [[ $_SYS == $LINUX_OS && $OS_BIT == 32 ]]; then
+	cmd=ossutil32
+else
+	echo "[ERROR]未知系统:"$SYS_UNAME",未知BIT:"$OS_BIT
+    exit
+fi
+
+# 脚本不存在则下载
+if [ ! -e "${cmd}" ]; then
+	echo "[INFO]下载脚本"${cmd}
+	if [[ $cmd="ossutilmac64" || $cmd="ossutilmac32" ]]; then
+		curl -o ${cmd} http://gosspublic.alicdn.com/ossutil/1.7.3/${cmd}
+	elif [[ $cmd="ossutil64" || $cmd="ossutil32" ]]; then
+		wget http://gosspublic.alicdn.com/ossutil/1.7.3/${cmd}
+	else
+		echo "[ERROR]未知脚本"${cmd}
+		exit
+	fi
+fi
+
+# 设置脚本执行权限
+if [ ! -x "${cmd}" ]; then
+	chmod 755 ${cmd}
+fi
+
+if [ $# -gt 0 ]; then
+	echo "[INFO]上传文件，共计：$#"
+	for i in "$@"; do
+		echo ">>>上传文件[$i]============================"
+	    ./${cmd} cp $i oss://${bucketAndFold} -c ${conf}
+	done
+	echo "[INFO]上传文件执行完毕！"
+else
+	echo "[ERROR]未指定上传文件"
+fi
+
+
